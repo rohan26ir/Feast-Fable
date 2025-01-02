941c604c19196c53f2655ce907620271749f8a24
Rohan <rohan26ir@gmail.com>
2025-01-02 20:57:57 +0600

    first commit

---
+++ src/Layout/MainLayout.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Outlet } from 'react-router-dom';
+
+const MainLayout = () => {
+  return (
+    <div>
+      
+
+      <div>
+        <Outlet></Outlet>
+      </div>
+
+    </div>
+  );
+};
+
+export default MainLayout;
